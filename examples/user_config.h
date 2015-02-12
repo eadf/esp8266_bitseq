011f5f04f0a15ad46fae37ccee421e2e24c174dd
EAD Fritz <ead.fritz@gmail.com>
2015-02-12 11:27:40 +0100

    Pulling in github.com:tuanpmt/esp_mqtt.git as a submodule

--- examples/user_config.h
+++ examples/user_config.h
@@ -25,4 +25,6 @@
 #define QUEUE_BUFFER_SIZE      2048
 #define SENSOR_SAMPLE_PERIOD   1000            // in milli-seconds, 1000 ms = 1 second
 
+#define PROTOCOL_NAMEv31	/*MQTT version 3.1 compatible with Mosquitto v0.15*/
+//PROTOCOL_NAMEv311			/*MQTT version 3.11 compatible with https://eclipse.org/paho/clients/testing/*/
 #endif
